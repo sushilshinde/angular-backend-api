4f9903feb1720f33d3e2b8d7056c4a70a9cea469
saikat-2022 <sacharjya@msystechnologies.com>
2023-08-22 16:46:58 +0530

    add user's backend data

--- routes/team-b.js
+++ routes/team-b.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const userData = require('../assets/team-b/users.json')
+fs = require('fs');
 
 router.get('/', (req, res) => {
   const teamAData = {
@@ -9,4 +11,26 @@ router.get('/', (req, res) => {
   res.json(teamAData);
 });
 
+const writeUserData = (jsonData) => {
+    try {
+      fs.writeFileSync(
+        "assets/team-b/users.json",
+        JSON.stringify(jsonData),
+        "utf8"
+      );
+    } catch (error) {
+      console.error("Error writing data:", error);
+    }
+  };
+
+router.post("/users", (req, res) => {
+    // console.log(req.body);
+    userData.users.push(req.body);
+    writeUserData(userData);
+    res.send({ userData });
+    console.log(userData);
+  });
+  router.get('/users',(req,res)=>{
+    res.json(userData.users);
+  })
 module.exports = router;
