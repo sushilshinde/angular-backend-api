c50427ce290feea1dfd73fa56fb9291eba2535c8
Murthy Kolati <kvarahamurty@msystechnologies.com>
2023-08-24 16:05:43 +0530

    added all tasks data

--- routes/team-b.js
+++ routes/team-b.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userData = require('../assets/team-b/users.json')
+const allTasksData = require('../assets/team-b/all_tasks.json')
 fs = require('fs');
 
 router.get('/', (req, res) => {
@@ -22,6 +23,17 @@ const writeUserData = (jsonData) => {
       console.error("Error writing data:", error);
     }
   };
+const writeAllTasksData = (jsonData) => {
+    try {
+      fs.writeFileSync(
+        "assets/team-b/all_tasks.json",
+        JSON.stringify(jsonData),
+        "utf8"
+      );
+    } catch (error) {
+      console.error("Error writing data:", error);
+    }
+  };
 
 router.post("/users", (req, res) => {
     // console.log(req.body);
@@ -33,4 +45,14 @@ router.post("/users", (req, res) => {
   router.get('/users',(req,res)=>{
     res.json(userData.users);
   })
+
+router.post("/all_tasks", (req,res) => {
+  allTasksData.all_tasks.push(req.body);
+  writeAllTasksData(allTasksData);
+  res.send({allTasksData});
+  console.log(allTasksData)
+});
+router.get('/all_tasks', (req,res) => {
+  res.json(allTasksData.all_tasks)
+})
 module.exports = router;
