87407f17760bcdfabe36ce6114a4c1bf13497199
nadaf-msys <nsharapoddin@msystechnologies.com>
2023-08-25 15:33:50 +0530

    Added /weather route and its api

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var cors = require('cors')
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -14,6 +16,7 @@ const teamDRoute = require('./routes/team-d');
 const teamERoute = require('./routes/team-e');
 
 var app = express();
+app.use(cors())
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -33,14 +36,13 @@ app.use('/team-c', teamCRoute);
 app.use('/team-d', teamDRoute);
 app.use('/team-e', teamERoute);
 
-
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- routes/team-a.js
+++ routes/team-a.js
@@ -1,20 +1,146 @@
+const citiesJson = require('../data/cities.json');
 const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
   const teamAData = {
     team: 'Team A',
-    message: 'Welcome to Team A endpoint!'
+    message: 'Welcome to Team A endpoint!',
   };
   res.json(teamAData);
 });
 
-router.get('/aa', (req, res) => {
-    const teamAData = {
-      team: 'Team AA',
-      message: 'Welcome to Team AA endpoint!'
+function findCityStateForLocation(location) {
+  for (const state in citiesJson) {
+    const cities = citiesJson[state];
+    const formattedLocation = location?.substring(0, 1).toUpperCase() + location.substring(1);
+    if (cities.includes(formattedLocation)) {
+      return [location, state];
+    }
+  }
+  return undefined;
+}
+
+function findWeatherForLocation(location, weatherData) {
+  const matchedCityState = findCityStateForLocation(location);
+  if (matchedCityState) {
+    const [city, state] = matchedCityState;
+    const weatherForLocation = {
+      ...weatherData,
+      city,
+      state,
+      country: 'India',
     };
-    res.json(teamAData);
-  });
+    return weatherForLocation;
+  }
+  return undefined;
+}
+
+router.get('/weather-details', (req, res) => {
+  let city = req.query.city;
+  city = city?.substring(0, 1).toUpperCase() + city.substring(1);
+  const weatherData = {
+    city,
+    state: 'Karnataka',
+    country: 'India',
+    weatherStatus: 'Sunny',
+    temperature: 30,
+    humidity: 12,
+    wind: 14,
+    precipitation: 13,
+    rainChance: 11,
+    rainChanceHourly: [
+      {
+        '6h': 50,
+      },
+      {
+        '12h': 37,
+      },
+      {
+        '18h': 46,
+      },
+      {
+        '24h': 68,
+      },
+    ],
+    windSpeedHourly: [
+      {
+        '6h': 30,
+      },
+      {
+        '12h': 10,
+      },
+      {
+        '18h': 20,
+      },
+      {
+        '24h': 15,
+      },
+    ],
+    temperatureHourly: [
+      {
+        '6h': 25,
+      },
+      {
+        '12h': 32,
+      },
+      {
+        '18h': 20,
+      },
+      {
+        '24h': 18,
+      },
+    ],
+    precipitationHourly: [
+      {
+        '6h': 12,
+      },
+      {
+        '12h': 5,
+      },
+      {
+        '18h': 7,
+      },
+      {
+        '24h': 19,
+      },
+    ],
+    temperatureWeekly: [
+      {
+        Mon: 30,
+      },
+      {
+        Tue: 35,
+      },
+      {
+        wed: 18,
+      },
+      {
+        Thu: 20,
+      },
+      {
+        Fri: 31,
+      },
+      {
+        Sat: 20,
+      },
+      {
+        Sun: 35,
+      },
+    ],
+  };
+  const getData = findWeatherForLocation(city, weatherData)
+
+  const teamAData = { ...getData };
+  res.json(teamAData);
+});
+
+router.get('/aa', (req, res) => {
+  const teamAData = {
+    team: 'Team AA',
+    message: 'Welcome to Team AA endpoint!',
+  };
+  res.json(teamAData);
+});
 
 module.exports = router;
