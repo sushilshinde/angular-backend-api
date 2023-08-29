d1d4436238d6ae9ec35437027f43e40d31888026
saikat-2022 <sacharjya@msystechnologies.com>
2023-08-29 16:34:27 +0530

    edit for search function

--- routes/team-b.js
+++ routes/team-b.js
@@ -13,46 +13,62 @@ router.get('/', (req, res) => {
 });
 
 const writeUserData = (jsonData) => {
-    try {
-      fs.writeFileSync(
-        "assets/team-b/users.json",
-        JSON.stringify(jsonData),
-        "utf8"
-      );
-    } catch (error) {
-      console.error("Error writing data:", error);
-    }
-  };
+  try {
+    fs.writeFileSync(
+      "assets/team-b/users.json",
+      JSON.stringify(jsonData),
+      "utf8"
+    );
+  } catch (error) {
+    console.error("Error writing data:", error);
+  }
+};
 const writeAllTasksData = (jsonData) => {
-    try {
-      fs.writeFileSync(
-        "assets/team-b/all_tasks.json",
-        JSON.stringify(jsonData),
-        "utf8"
-      );
-    } catch (error) {
-      console.error("Error writing data:", error);
-    }
-  };
+  try {
+    fs.writeFileSync(
+      "assets/team-b/all_tasks.json",
+      JSON.stringify(jsonData),
+      "utf8"
+    );
+  } catch (error) {
+    console.error("Error writing data:", error);
+  }
+};
 
 router.post("/users", (req, res) => {
-    // console.log(req.body);
-    userData.users.push(req.body);
-    writeUserData(userData);
-    res.send({ userData });
-    console.log(userData);
-  });
-  router.get('/users',(req,res)=>{
-    res.json(userData.users);
-  })
+  // console.log(req.body);
+  userData.users.push(req.body);
+  writeUserData(userData);
+  res.send({ userData });
+  console.log(userData);
+});
+router.get('/users', (req, res) => {
+  res.json(userData.users);
+})
 
-router.post("/all_tasks", (req,res) => {
+router.post("/all_tasks", (req, res) => {
   allTasksData.all_tasks.push(req.body);
   writeAllTasksData(allTasksData);
-  res.send({allTasksData});
+  res.send({ allTasksData });
   console.log(allTasksData)
 });
-router.get('/all_tasks', (req,res) => {
-  res.json(allTasksData.all_tasks)
+
+router.get('/all_tasks', (req, res) => {
+  const foundItem = allTasksData.all_tasks.filter(
+    (item) => {
+
+      return item.tasks.filter(item => item.title.toLowerCase().includes(req.query.q)).length > 0
+    }
+  )
+
+  const mappedTask = foundItem.map(item => item.tasks).flat();
+
+  const foundTask = mappedTask.filter(taskItem => taskItem.title.toLowerCase().includes(req.query.q));
+
+  res.json(foundTask)
 })
+
+
+
+
 module.exports = router;
